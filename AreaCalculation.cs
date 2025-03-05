7f44f8b045270f6361df2bfcd73e399f0644f417
BhangayAvanthi <137265521+BhangayAvanthi@users.noreply.github.com>
2025-03-05 16:05:00 +0530

    Add files via upload

---
+++ AreaCalculation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//**Abstraction with Abstract Classes**
+
+//Design an abstract class `Shape` with an abstract method `CalculateArea()`.
+//Create derived classes `Circle` and `Rectangle` that implement this method.
+//Demonstrate abstraction by instantiating these classes.
+
+namespace practise;
+
+abstract class Shape
+{
+    public abstract void Area();
+}
+
+class Circle : Shape
+{
+    int radius;
+    public Circle(int radius)
+    {
+        this.radius = radius;
+    }
+    public override void Area()
+    {
+        Console.WriteLine("Circle Area::"+(Math.PI*
+            radius* radius));
+    }
+}
+
+class Rectangle : Shape
+{
+    private int length, breadth;
+
+    public Rectangle(int length, int breadth)
+    {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public override void Area()
+    {
+        Console.WriteLine("Rectangle Area::" +(length*breadth));
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using practise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//**Method Overriding for a Vehicle System**
+
+//Create a base class `Vehicle` with a method `Start()`.
+//Override it in `Car` and `Bike` classes to provide specific implementations.
+//Use the `override` keyword and demonstrate polymorphism.
+
+namespace practise
+{
+    class Vehicle
+    {
+        public void Start()
+        {
+            Console.WriteLine("starting");
+        }
+    }
+    class car
+    {
+        public void Start()
+        {
+            Console.WriteLine("car started");
+        }
+    }
+    class bike
+    {
+        public void Start()
+        {
+            Console.WriteLine("bike started");
+        }
+    }
+}
+
+
