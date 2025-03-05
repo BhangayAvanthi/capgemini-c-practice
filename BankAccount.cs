7b876d6fea2a7136725d8f58dcf22199e51c2985
BhangayAvanthi <137265521+BhangayAvanthi@users.noreply.github.com>
2025-03-05 14:43:14 +0530

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//**Encapsulation Challenge**
+
+//Create a `BankAccount` class where `balance` is a private field.Implement methods for `Deposit()` and `Withdraw()`, 
+//ensuring that withdrawal is only allowed if there is a sufficient balance.Prevent direct modification of `balance` from outside the class.
+
+namespace practise
+{
+    class BankAccount
+    {
+        private decimal balance=50000;
+        public void HolderInfo()
+        {
+            string Name,AccNumber;
+            Console.WriteLine("enter name ::");
+            Name = Console.ReadLine();
+            Console.WriteLine("enter acc numb ::");
+            AccNumber = Console.ReadLine();
+            Console.WriteLine("Balance::" + balance);
+        }
+
+        public void WithDraw()
+        {
+            decimal Amount;
+            Console.Write("enter amount to withdraw:: ");
+            Amount=Convert.ToDecimal(Console.ReadLine());
+            if (Amount <= balance)
+            {
+                balance = balance - Amount;
+                Console.WriteLine("total balance=" + balance);
+            }
+            else if (Amount > balance)
+            {
+                Console.WriteLine("unsufficient balance!! try again!!");
+                WithDraw();
+            }
+        }
+
+        public void Deposit()
+        {
+            decimal Amount;
+            Console.WriteLine("enter amount to deposit ::");
+            Amount = Convert.ToDecimal(Console.ReadLine());
+            balance = balance + Amount;
+            Console.WriteLine("total balance == " + balance);
+        }
+    }
+}
---
+++ Books.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//**Constructor Overloading in a Library System**
+
+//Implement a `Book` class with three different constructors:
+//(1) Default constructor,
+//(2) Constructor with `Title` and `Author`,
+//(3) Constructor with `Title`, `Author`, and `ISBN`.
+//Ensure each constructor initializes properties correctly.
+
+namespace practise
+{
+    class Books
+    {
+        public Books()
+        {
+            Console.WriteLine("Books Default Constructor");
+        }
+        public Books(string Title,string Author)
+        {
+            Console.WriteLine($"Title::{Title}");
+            Console.WriteLine($"Author::{Author}");
+        }
+        public Books(string Title,string Author,int ISBN)
+        {
+            Console.WriteLine($"Title::{Title}");
+            Console.WriteLine($"Author::{Author}");
+            Console.WriteLine($"ISBN::{ISBN}");
+        }
+
+    }
+}
