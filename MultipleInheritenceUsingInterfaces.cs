855a18373832564997bf3d897bf233f7785c8312
BhangayAvanthi <137265521+BhangayAvanthi@users.noreply.github.com>
2025-03-06 10:59:14 +0530

    Add files via upload

---
+++ MultipleInheritenceUsingInterfaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//**Multiple Inheritance Using Interfaces**
+//Define two interfaces `IPrintable` (for printing details) and `ISerializable` (for saving to a file).
+//Implement both in a `Report` class and demonstrate multiple interface implementation.
+
+
+namespace practise
+{
+    interface IPrintable
+    {
+        void Print();
+    }
+    interface ISerializable
+    {
+        void Save(string filename);
+    }
+    class Report:IPrintable,ISerializable
+    {
+        string Title, Content;
+
+        public Report(string title,string content)
+        {
+            Title = title;
+            Content = content;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Report Info::\nTitle={Title}\nContent={Content}");
+        }
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, Content);
+            Console.WriteLine($"Report is saved as {filename}");
+        }
+    }
+}
---
+++ Role-based-Access.cs
@@ -0,0 +1,54 @@
+﻿using practise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//**Design a Role-Based Access System**
+//Create a base class `User` with properties like `Username` and `Role`.
+//Derive `Admin` and `Customer` classes that override a method `AccessControl()`,
+//where `Admin` can access all features, but `Customer` has limited access.
+
+namespace practise
+{
+    
+    class User
+    {
+        protected string Username, Role;
+
+        public User(string username,string role)
+        {
+            Username = username;
+            Role = role;
+        }
+
+        public virtual void AccessControl()
+        {
+            Console.WriteLine($"{Username}::{Role}\nAccent isn't defined");
+        }
+    }
+    class Admin:User
+    {
+        public Admin(string Username) : base(Username, "Admin")
+        {
+
+        }
+        public override void AccessControl()
+        {
+            Console.WriteLine($"{Username}[{Role}]: You have full access to the system.");
+        }
+    }
+
+    class Customer : User
+    {
+        public Customer(string username) : base(username,"Customer")
+        {
+        }
+        public override void AccessControl()
+        {
+            Console.WriteLine($"{Username}[{Role}]: You don't have access to the system.");
+        }
+    }
+}
