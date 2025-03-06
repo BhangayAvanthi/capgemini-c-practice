2cc7bfbf09dbfe671c1d6a53f007cf3e96a7cb52
BhangayAvanthi <137265521+BhangayAvanthi@users.noreply.github.com>
2025-03-06 13:42:51 +0530

    Add files via upload

---
+++ SecuritySystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise
+{
+    sealed class SecuritySystem
+    {
+        public void AuthenticateUser(string username, string password)
+        {
+            Console.WriteLine($"Authenticating {username}...");
+            Console.WriteLine("Access Granted!");
+        }
+    }
+    //SecuritySystem class cannot be inherited becoz of sealed keyword
+    //class AdvancedSecurity : SecuritySystem
+    //{
+    //    Console.WriteLine("hey..");
+    //}
+}
