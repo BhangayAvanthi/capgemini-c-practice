8c256300c4c583d82559ac97aca2b448a30a1072
BhangayAvanthi <137265521+BhangayAvanthi@users.noreply.github.com>
2025-03-06 10:15:32 +0530

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise
+{
+    interface IPlayable
+    {
+        void Play();
+    }
+    class MusicPlayer:IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("playing music");
+        }
+    }
+    class VideoPlayer:IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("video playing");
+        }
+    }
+}
