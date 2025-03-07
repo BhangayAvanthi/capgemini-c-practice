76cffd31005ab4f3653817269b6f37550c84f4d6
BhangayAvanthi <137265521+BhangayAvanthi@users.noreply.github.com>
2025-03-07 11:56:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+using practise;
+
+Console.WriteLine("Hello, World!");
+
+
+//Bike ob1 = new Bike();
+//ob1.BInfo();
+//ob1.DisplayInfo();
+//Car ob2 = new Car();
+//ob2.CInfo();
+//ob2.DisplayInfo();
+
+//BankAccount obj = new BankAccount();
+//obj.HolderInfo();
+//obj.WithDraw();
+//obj.Deposit();
+
+//StudentCredentials obj = new StudentCredentials();
+//obj.StudentInfo();
+
+//Books book = new Books();
+//Books book1 = new Books("xyz", "zxc");
+//Books book2 = new Books("dcxr", "rfdr", 1234567);
+
+//Circle circle = new Circle(2);
+//circle.Area();
+//Rectangle rectangle = new Rectangle(2, 4);
+//rectangle.Area();
+
+//Vehicle vehicle = new Vehicle();
+//vehicle.Start();
+//car obj1 = new car();
+//obj1.Start();
+//bike obj2 = new bike();
+//obj2.Start();
+
+//PersonInfo person = new PersonInfo("person1",12);
+//person.GetDetails();
+//Teacher teacher = new Teacher("Teacher 1", 30, "Python");
+//teacher.GetDetails();
+//Student student = new Student("Student 1", 13, 1234, 7);
+//student.GetDetails();
+
+//Calculator calculator = new Calculator();
+//calculator.Add(2, 4);
+//calculator.Add(2, 4, 2);
+//calculator.Add(2.4, 2.4);
+
+//MusicPlayer music = new MusicPlayer();
+//music.Play();
+//VideoPlayer video= new VideoPlayer();
+//video.Play();
+
+//Report report = new Report("my report title", "my report cnotent");
+//report.Print();
+//report.Save("myreport.txt");
+
+//Admin admin = new Admin("HOD");
+//admin.AccessControl();
+//Customer customer = new Customer("Studnet");
+//customer.AccessControl();
+
+//ComplexNumbers cn1 = new ComplexNumbers(1, 2);
+//ComplexNumbers cn2 = new ComplexNumbers(1, 2);
+//Console.WriteLine($"sum::{cn1+cn2}");
+
+//SecuritySystem securitySystem = new SecuritySystem();
+//securitySystem.AuthenticateUser("user", "password123");
+
